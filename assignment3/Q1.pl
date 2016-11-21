3fd1a4611d9cda2fdd74711aa296683d5e137359
Rex Low <qiweilow950823@gmail.com>
2016-11-22 03:34:20 +0800

    Added simple if else example

---
+++ assignment3/Q1.pl
@@ -0,0 +1,9 @@
+main:-
+  write('Hello, please enter your electricity usage: '), read(Usage),
+  find_usage(Usage, Amount),
+  write(Amount).
+
+find_usage(Usage, Amount):-
+
+
+  
---
+++ ifelse.pl
@@ -0,0 +1,1 @@
+max(X,Y,Z) :- X>Y -> Z is X ; Z is Y.
