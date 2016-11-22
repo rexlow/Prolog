7cdb4ee9dc29a8718a94daaa907b3ef8177888c8
Kon Yee <konyee95@gmail.com>
2016-11-22 14:30:47 +0800

    Completed question 1

--- assignment3/Q1.pl
+++ assignment3/Q1.pl
@@ -1,9 +1,19 @@
-main:-
-  write('Hello, please enter your electricity usage: '), read(Usage),
-  find_usage(Usage, Amount),
-  write(Amount).
+main:-ask_usage(Usage).
 
-find_usage(Usage, Amount):-
+amount_bill(Usage):-
+	Usage>0,
+	Usage=<150,
+	Amount is Usage*0.15,
+	write('Your bill amount usage is:'),
+	write(Amount),!.
 
+amount_bill(Usage):-
+	Usage>150,
+	Z is Usage-150,
+	Amount is (150*0.15+Z*0.2),
+	write('Your bill amount usage is:'),
+	write(Amount),!.
 
-  
+ask_usage(Usage):- write('Hallo,please enter your electricity usage: '),
+	read(Usage),
+	amount_bill(Usage).
