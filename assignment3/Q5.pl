672e829646054afd72406192bbcfbb850d10dbde
Rex Low <qiweilow950823@gmail.com>
2016-11-27 16:06:34 +0800

    Completed Assignment3

--- assignment3/Q5.pl
+++ assignment3/Q5.pl
@@ -1,188 +1,132 @@
-%Error, red cut used in (b) due to complicated rules
-%Pending to change
+%a Write a Prolog Fact for each book
+% declare dynamic facts
+:- dynamic title/2.
+:- dynamic author/2.
+:- dynamic publisher/2.
+:- dynamic price/2.
+:- dynamic call_number/2.
+:- dynamic edition/2.
+:- dynamic data_disk/2.
 
-%Question 5
-%(a)
-title(book1, 'Elementary Statistics').
-title(book2, 'Statistics for Engineers').
-title(book3, 'Statistics for Engineers and Scientists').
-title(book4, 'IT in Language Learning').
+% clear KB everytime it compiles
+:- retractall(title(_,_)).
+:- retractall(author(_,_)).
+:- retractall(publisher(_,_)).
+:- retractall(price(_,_)).
+:- retractall(call_number(_,_)).
+:- retractall(edition(_,_)).
+:- retractall(data_disk(_,_)).
 
-author(book1, 'Patricia Wilson').
-author(book2, 'James Mori').
-author(book3, 'James Mori').
-author(book4, 'H Basiron').
+% assert facts.
+:- assert(title(book1, 'Elementary Statistics')).
+:- assert(title(book2, 'Statistics for Engineers')).
+:- assert(title(book3, 'Statistics for Engineers and Scientists')).
+:- assert(title(book4, 'IT in Language Learning')).
 
-publisher(book1, 'Addison Wesley').
-publisher(book2, 'World Scientific').
-publisher(book3, 'World Scientific').
-publisher(book4, 'UTeM Press').
+:- assert(author(book1, 'Patricia Wilson')).
+:- assert(author(book2, 'James Mori')).
+:- assert(author(book3, 'James Mori')).
+:- assert(author(book4, 'H Basiron')).
 
-price(book1, 75).
-price(book2, 125).
-price(book3, 125).
-price(book4, 5).
+:- assert(publisher(book1, 'Addison Wesley')).
+:- assert(publisher(book2, 'World Scientific')).
+:- assert(publisher(book3, 'World Scientific')).
+:- assert(publisher(book4, 'UTeM Press')).
 
-call_number(book1, 'QA373').
-call_number(book2, 'QA673').
-call_number(book3, 'QA674').
-call_number(book4, 'QA007').
+:- assert(price(book1, 75)).
+:- assert(price(book2, 125)).
+:- assert(price(book3, 125)).
+:- assert(price(book4, 5)).
 
-edition(book1, 1).
-edition(book2, 3).
-edition(book3, 2).
-edition(book4, 1).
+:- assert(call_number(book1, 'QA373')).
+:- assert(call_number(book2, 'QA673')).
+:- assert(call_number(book3, 'QA674')).
+:- assert(call_number(book4, 'QA007')).
 
-data_disk(book1, 'No').
-data_disk(book2, 'Yes').
-data_disk(book3, 'Yes').
-data_disk(book4, 'No').
+:- assert(edition(book1, 1)).
+:- assert(edition(book2, 3)).
+:- assert(edition(book3, 2)).
+:- assert(edition(book4, 1)).
 
-%(b)
+:- assert(data_disk(book1, 'No')).
+:- assert(data_disk(book2, 'Yes')).
+:- assert(data_disk(book3, 'Yes')).
+:- assert(data_disk(book4, 'No')).
+
+%b The procedure should return the Title, Price and Call number of the book with data diskettes.
 check_disk(Title, Price, CallNo) :-
-	data_disk(Book, 'Yes'),
-	title(Book, Title),
-	price(Book, Price),
-	call_number(Book, CallNo).
+  data_disk(Book, 'Yes'),
+  title(Book, Title),
+  price(Book, Price),
+  call_number(Book, CallNo).
 
-%to check the book detail
-book_detail(Book) :-
-	title(Book, Title),
-	author(Book, Author),
-	publisher(Book, Publisher),
-	price(Book, Price),
-	call_number(Book, CallNo),
-	edition(Book, Edition),
-	data_disk(Book, Data_disk),
-	write('Title: '), writeln(Title),
-	write('Author: '), writeln(Author),
-	write('Publisher: '), writeln(Publisher),
-	write('Price: '), writeln(Price),
-	write('Call number: '), writeln(CallNo),
-	write('Edition: '), writeln(Edition),
-	write('Data disk: '), writeln(Data_disk), !.
+%c Define a rule in which you are able to add the book information dynamically.
+add_book :-
+  aggregate_all(count, title(_,_), Count),
+  NewCount is Count + 1,
+  atom_concat('book', NewCount, NewBook),
 
-%(c)
-title(X, Y) :- a(X, Y), !.
-author(X, Y) :- b(X, Y), !.
-publisher(X, Y) :- c(X, Y), !.
-price(X, Y) :- d(X, Y), !.
-call_number(X, Y) :- e(X, Y), !.
-edition(X, Y) :- f(X, Y), !.
-data_disk(X, Y) :- g(X, Y), !.
+  write('Enter book title: '), readln(Title),
+  write('Enter book author: '), readln(Author),
+  write('Enter book publisher: '), readln(Publisher),
+  write('Enter book price: '), readln(Price),
+  write('Enter book call number: '), readln(CallNo),
+  write('Enter book edition: '), readln(Edition),
+  write('Data disk available? (Yes, No): '), readln(DataDisk),
 
+  atomic_list_concat(Title, ' ', NewTitle),
+	atomic_list_concat(Author, ' ', NewAuthor),
+	atomic_list_concat(Publisher, ' ', NewPublisher),
+	atomic_list_concat(Price, ' ', NewPrice),
+	atomic_list_concat(CallNo, ' ', NewCallNo),
+	atomic_list_concat(Edition, ' ', NewEditon),
+	atomic_list_concat(DataDisk, ' ', NewDataDisk),
 
-add_book() :-
-	book_increase(),
-	book(Book),
-	atom_concat('book', Book, X),
-	set_assert_rule(),
-	writeln(X),
+  assert(title(NewBook, NewTitle)),
+  assert(author(NewBook, AuthNewAuthoror)),
+  assert(publisher(NewBook, NewPublisher)),
+  assert(price(NewBook, NewPrice)),
+  assert(call_number(NewBook, NewCallNo)),
+  assert(edition(NewBook, NewEdition)),
+  assert(data_disk(NewBook, NewDataDisk)), !.
 
-	write('Enter Title: '), readln(Title),
-	write('Enter Author: '), readln(Author),
-	write('Enter Publisher: '), readln(Publisher),
-	write('Enter Price: '), readln(Price),
-	write('Enter Call number: '), readln(CallNo),
-	write('Enter Edition: '), readln(Edition),
-	write('Enter Data disk: '), readln(Data_disk),
+%d Define a rule in which you are able to write the book information in an output file.
+file :-
+  aggregate_all(count, title(_,_), Count),
+  NewCount is Count + 1,
+  atom_concat('book', NewCount, NewBook),
 
-	atomic_list_concat(Title, ' ', A),
-	atomic_list_concat(Author, ' ', B),
-	atomic_list_concat(Publisher, ' ', C),
-	atomic_list_concat(Price, ' ', D),
-	atomic_list_concat(CallNo, ' ', E),
-	atomic_list_concat(Edition, ' ', F),
-	atomic_list_concat(Data_disk, ' ', G),
+  write('Enter book title: '), readln(Title),
+  write('Enter book author: '), readln(Author),
+  write('Enter book publisher: '), readln(Publisher),
+  write('Enter book price: '), readln(Price),
+  write('Enter book call number: '), readln(CallNo),
+  write('Enter book edition: '), readln(Edition),
+  write('Data disk available? (Yes, No): '), readln(DataDisk),
 
-	asserta(a(X, A)),
-	asserta(b(X, B)),
-	asserta(c(X, C)),
-	asserta(d(X, D)),
-	asserta(e(X, E)),
-	asserta(f(X, F)),
-	asserta(g(X, G)), !.
+  atomic_list_concat(Title, ' ', NewTitle),
+  atomic_list_concat(Author, ' ', NewAuthor),
+  atomic_list_concat(Publisher, ' ', NewPublisher),
+  atomic_list_concat(Price, ' ', NewPrice),
+  atomic_list_concat(CallNo, ' ', NewCallNo),
+  atomic_list_concat(Edition, ' ', NewEditon),
+  atomic_list_concat(DataDisk, ' ', NewDataDisk),
 
-:- dynamic(book/1).
-:- dynamic(status/1).
+  assert(title(NewBook, NewTitle)),
+  assert(author(NewBook, NewAuthor)),
+  assert(publisher(NewBook, NewPublisher)),
+  assert(price(NewBook, NewPrice)),
+  assert(call_number(NewBook, NewCallNo)),
+  assert(edition(NewBook, NewEdition)),
+  assert(data_disk(NewBook, NewDataDisk)),
 
-book_increase() :-
-	assert(book(4)),
-	(   book(X), X \= 4
-	->  Y is X + 1,
-	    retractall(book(_)),
-	    assert(book(Y))
-	;   assert(book(5)),
-	    retract(book(4))
-	).
-
-
-set_assert_rule() :-
-	assert(status(false)),
-	(   status(true)
-	->  true
-	;   assert(status(true)),
-
-	    assert((a(X, Y) :- title(X, Y), !)),
-	    assert((b(X, Y) :- author(X, Y), !)),
-	    assert((c(X, Y) :- publisher(X, Y), !)),
-	    assert((d(X, Y) :- price(X, Y), !)),
-	    assert((e(X, Y) :- call_number(X, Y), !)),
-	    assert((f(X, Y) :- edition(X, Y), !)),
-	    assert((g(X, Y) :- data_disk(X, Y), !))
-	),
-	retract(status(false)).
-
-%(d)
-add_to_file(Book) :-
-	title(Book, Title),
-	author(Book, Author),
-	publisher(Book, Publisher),
-	price(Book, Price),
-	call_number(Book, CallNo),
-	edition(Book, Edition),
-	data_disk(Book, Data_disk),
-
-	open('BookInfo.txt', write, OutStream),
-	writeln(OutStream, Book),
-	writeln(OutStream, Title),
-	writeln(OutStream, Author),
-	writeln(OutStream, Publisher),
-	writeln(OutStream, Price),
-	writeln(OutStream, CallNo),
-	writeln(OutStream, Edition),
-	writeln(OutStream, Data_disk),
+  open('Books.txt', write, OutStream),
+  writeln(OutStream, NewBook),
+	writeln(OutStream, NewTitle),
+	writeln(OutStream, NewAuthor),
+	writeln(OutStream, NewPublisher),
+	writeln(OutStream, NewPrice),
+	writeln(OutStream, NewCallNo),
+	writeln(OutStream, NewEditon),
+	writeln(OutStream, NewDataDisk),
 	close(OutStream).
-
-add_to_file() :-
-	book_increase(),
-	book(Book),
-	atom_concat('book', Book, X),
-
-	write('Enter Title: '), readln(Title),
-	write('Enter Author: '), readln(Author),
-	write('Enter Publisher: '), readln(Publisher),
-	write('Enter Price: '), readln(Price),
-	write('Enter Call number: '), readln(CallNo),
-	write('Enter Edition: '), readln(Edition),
-	write('Enter Data disk: '), readln(Data_disk),
-
-	atomic_list_concat(Title, ' ', A),
-	atomic_list_concat(Author, ' ', B),
-	atomic_list_concat(Publisher, ' ', C),
-	atomic_list_concat(Price, ' ', D),
-	atomic_list_concat(CallNo, ' ', E),
-	atomic_list_concat(Edition, ' ', F),
-	atomic_list_concat(Data_disk, ' ', G),
-
-	open('BookInfo.txt', write, OutStream),
-	writeln(OutStream, X),
-	writeln(OutStream, A),
-	writeln(OutStream, B),
-	writeln(OutStream, C),
-	writeln(OutStream, D),
-	writeln(OutStream, E),
-	writeln(OutStream, F),
-	writeln(OutStream, G),
-	close(OutStream).
-
