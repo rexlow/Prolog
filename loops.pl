97667c678919601b6cb74ce8e05365aaa28d9368
Rex Low <qiweilow950823@gmail.com>
2016-12-26 18:20:41 +0800

    added failure driven loop

--- loops.pl
+++ loops.pl
@@ -1,5 +1,16 @@
+%
 % simple recursion
+%
 recurse([]).
 recurse([H|T]) :-
   writeln(H),
   recurse(T).
+
+%
+%failure driven loop
+%
+fail_driven(List) :-
+  member(X, List),
+  writeln(X),
+  fail.
+fail_driven(_).
