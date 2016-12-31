988ec934905a8520f8fa34c56957fe7028390337
Rex Low <qiweilow950823@gmail.com>
2017-01-01 04:37:03 +0800

    updated example for at-end-of-stream

---
+++ At-End-of-Stream/redcut.pl
@@ -0,0 +1,12 @@
+main :-
+  open('houses.txt', read, Stream),
+  readContent(Stream, Houses),
+  close(Stream),
+  write(Houses), nl.
+
+readContent(Stream, []) :-
+  at_end_of_stream(Stream), !.
+
+readContent(Stream, [H|T]) :-
+  read(Stream, H),
+  readContent(Stream, T).
